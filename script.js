f8249947c46056f3e0a81b63fdae65adc3140153
Johnpii1 <okolichimobi08gmail.com>
2025-11-10 20:45:18 +0100

    adding language

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//HEAD for mobile
+const btns = document.getElementById("btns");
+const lan = document.getElementById("langs")
+
+btns.addEventListener("click", () => {
+    lan.classList.toggle("show");
+});
+
+//HEAD for laptop
+const btn1 = document.getElementById("btn1");
+const lang1 = document.getElementById("langs1")
+
+btn1.addEventListener("click", () => {
+    lang1.classList.toggle("show");
+});
+
+
+//Heder
 const btn = document.getElementById("dropdown-btn");
 const contents = document.getElementById("menu");
 
